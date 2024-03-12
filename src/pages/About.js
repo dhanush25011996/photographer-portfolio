085f23a37d1f94bf6b83d1c6aa1bb8a9fd69c373
Dhanush Kodi <dhanushvijayakumar@gmail.com>
2024-03-12 18:33:30 +0530

    initial

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useContext } from "react";
+
+import WomanImg from "../img/about/woman.png";
+
+import { Link } from "react-router-dom";
+
+import { motion } from "framer-motion";
+
+import { transition1 } from "../transitions";
+
+import {CursorContext} from "../context/CursorContext.js";
 
 const About = () => {
-  return <div>About</div>;
+  const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
+  return (
+    <motion.section
+      initial={{ opacity: 0, y: "100%" }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: "100%" }}
+      transition={transition1}
+      className="section"
+    >
+      <div
+        onMouseEnter={mouseEnterHandler}
+        onMouseLeave={mouseLeaveHandler}
+        className="container mx-auto h-full relative"
+      >
+        <div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16">
+          <div className="flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden">
+            <img src={WomanImg} alt="womanimg" />
+          </div>
+
+          <motion.div
+            initial={{ opacity: 0, y: "-80%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "-80%" }}
+            transition={transition1}
+            className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start"
+          >
+            <h1 className="h1">About me</h1>
+            <p className="mb-12 max-w-sm">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. <b>Lorem Ipsum</b> has been the industry's standard
+              dummy text ever since the 1500s, when an unknown printer took a
+              galley of type and scrambled it to make a type specimen book.
+              <br />
+              <br />
+              It has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <Link to={"/portfolio"} className="btn">
+              View my work
+            </Link>
+          </motion.div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default About;
--- src/transitions.js
+++ src/transitions.js
@@ -3,3 +3,4 @@ export const transition1 = {
   ease: [0.6, 0.01, -0.05, 0.9],
   // ease: [0.43, 0.13, 0.23, 0.96],
 };
+
